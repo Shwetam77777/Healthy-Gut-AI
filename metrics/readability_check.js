02498c81e57d7fdf5e838e06857902c2915b87e4
Shweta mishra <111523337+Shwetam77777@users.noreply.github.com>
2025-11-24 14:17:39 +0530

    Update readability_check.js

--- metrics/readability_check.js
+++ metrics/readability_check.js
@@ -1,1 +1,28 @@
 
+function countSentences(text) {
+  const matches = text.match(/[.!?]/g);
+  return matches ? matches.length : 1;
+}
+
+function countSyllables(word) {
+  const m = word.toLowerCase().match(/[aeiouy]+/g);
+  return m ? m.length : 1;
+}
+
+function fleschReadingEase(text) {
+  const words = text.split(/\s+/).filter(Boolean);
+  const numWords = words.length || 1;
+  const numSentences = countSentences(text);
+  const numSyllables = words.reduce((sum, w) => sum + countSyllables(w), 0);
+
+  const ASL = numWords / numSentences;
+  const ASW = numSyllables / numWords;
+
+  return 206.835 - 1.015 * ASL - 84.6 * ASW;
+}
+
+const article = $json.optimized_article_markdown || "";
+
+return {
+  fleschReadingEase: Number(fleschReadingEase(article).toFixed(2))
+};
